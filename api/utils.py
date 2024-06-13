5b1a2f8e5617b49b3095bacb4b1c22b083bea209
Roman <yareskoempire69@gmail.com>
2024-06-13 13:09:27 +0200

    Added: small refactoring

--- api/utils.py
+++ api/utils.py
@@ -4,16 +4,17 @@ from PIL import Image, ImageOps
 class UserImageUtils():
     def borderize_image(image, color):     
         width, height = image.size
+        edited_image = image
         if width > height:
             edited_image = Image.new(image.mode, [width, width], color)
             edited_image.paste(image, [0, (width - height) // 2])
-            edited_image = ImageOps.expand(edited_image, width // 10, color)
+            edited_image = ImageOps.expand(edited_image, width // 25, color)
         elif width < height:
             edited_image = Image.new(image.mode, [height, height], color)
             edited_image.paste(image, [(height - width) // 2, 0])
-            edited_image = ImageOps.expand(edited_image, height // 10, color)
+            edited_image = ImageOps.expand(edited_image, height // 25, color)
         else:
-            edited_image = ImageOps.expand(edited_image, width // 10, color)
+            edited_image = ImageOps.expand(edited_image, width // 25, color)
 
         edited_image.format = image.format
         return edited_image
